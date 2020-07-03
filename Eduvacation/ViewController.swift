6424d020b170746c8fb2deffe71499da1b3acc9e
AshwinRajesh <ashwinr2003@yahoo.com>
2020-07-02 18:59:33 -0700

    Initial Commit

---
+++ Eduvacation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eduvacation
+//
+//  Created by Ashwin Rajesh on 7/2/20.
+//  Copyright © 2020 AshwinR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
